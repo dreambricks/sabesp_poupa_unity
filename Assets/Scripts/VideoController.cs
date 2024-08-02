6355b5e7b1b185757b43a6f706ebc580d3f44533
Julio Balbino <julio4110@hotmail.com>
2024-08-02 20:07:14 -0300

    chore: inial files

---
+++ Assets/Scripts/VideoController.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+
+    public Texture idleTexture;
+    public Texture playTexture;
+
+    public VideoPlayer idlePlayer;
+    public VideoPlayer playPlayer;
+
+    private RawImage panel;
+
+    //public ArduinoCommunication arduinoCommunication;
+
+    private void Start()
+    {
+        panel = GetComponent<RawImage>();
+        panel.texture = idleTexture;
+
+        if (playPlayer != null)
+        {
+            playPlayer.loopPointReached += OnVideoEnd;
+        }
+        else
+        {
+            Debug.LogError("VideoPlayer não está atribuído.");
+        }
+
+        StartIdleVideo();
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (!playPlayer.isPlaying)
+        {
+            SaveLog();
+            //arduinoCommunication.SendMessageToArduino("1");
+            StartPlayVideo();
+        }
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        //arduinoCommunication.SendMessageToArduino("0");
+        StartIdleVideo();
+    }
+
+    void StartIdleVideo()
+    {
+        idlePlayer.gameObject.SetActive(true);
+        idlePlayer.Stop();
+        idlePlayer.Play();
+        panel.texture = idleTexture;
+        playPlayer.gameObject.SetActive(false);
+    }
+
+    void StartPlayVideo()
+    {
+        playPlayer.gameObject.SetActive(true);
+        playPlayer.Stop();
+        playPlayer.Play();
+        panel.texture = playTexture;
+        idlePlayer.gameObject.SetActive(false);
+    }
+
+    void SaveLog()
+    {
+        DataLog dataLog = LogUtil.GetDatalogFromJson();
+        dataLog.status = StatusEnum.Jogou.ToString();
+        dataLog.additional = "vazio";
+        LogUtil.SaveLog(dataLog);
+    }
+}
